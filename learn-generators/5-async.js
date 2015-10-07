7e1d8d8c984543ebb5feefe16bfb6de03448ff1b
Mikhail Baranov <mikhail.baranov@gmail.com>
2015-10-08 01:34:23 +0300

    Done 5. LOOK SYNC. DO ASYNC.

---
+++ learn-generators/5-async.js
@@ -0,0 +1,26 @@
+var fs = require('fs');
+
+function run (generator) {
+	var it = generator(go);
+
+	function go (err, result) {
+		if (err) {
+			it.throw(err);
+		}
+		it.next(result);
+	}
+
+	go();
+}
+
+run(function* (done) {
+	var firstFile;
+	try {
+		var dirFiles = yield fs.readdir('NoNoNoNo', done); // No such dir
+		firstFile = dirFiles[0]; // TypeError: Cannot read property '0' of undefined
+	} catch (e) {
+		firstFile = null;
+	}
+
+	console.log(firstFile);
+});
